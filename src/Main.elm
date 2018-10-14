e4d438300e1816c30d9b8fcfc32f01db7d1ccec8
abab <ababup1192@gmail.com>
2018-10-14 12:16:44 +0900

    Create MonsterViewModel

--- src/Main.elm
+++ src/Main.elm
@@ -18,6 +18,11 @@ init _ =
     ( {}, Cmd.none )
 
 
+infinity : Float
+infinity =
+    1 / 0
+
+
 
 ---- UPDATE ----
 
@@ -29,6 +34,38 @@ type Msg
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     ( model, Cmd.none )
+
+
+type alias Monster =
+    { name : String, hp : Int, mp : Float, attack : Int, agility : Int }
+
+
+slime =
+    Monster "スライム" 8 0 9 4
+
+
+ogarasu =
+    Monster "おおがらす" 9 0 10 6
+
+
+samayouYoroi =
+    Monster "さまようよろい" 55 0 47 10
+
+
+druido =
+    Monster "ドルイド" 35 10 55 29
+
+
+hagreMetal =
+    Monster "はぐれメタル" 6 infinity 55 150
+
+
+zoma =
+    Monster "ゾーマ" 4700 infinity 360 80
+
+
+type alias MonsterViewModel =
+    { name : String, hp : String, mp : String, attack : String, agility : String }
 
 
 
@@ -60,50 +97,26 @@ view model =
                     ]
                 ]
             ]
-        , tbody []
-            [ tr []
-                [ td [] [ text "スライム" ]
-                , td [] [ text "8" ]
-                , td [] [ text "0" ]
-                , td [] [ text "9" ]
-                , td [] [ text "4" ]
+        , tbody [] <|
+            List.map monsterViewModel2View
+                [ MonsterViewModel "スライム" "8" "0" "9" "4"
+                , MonsterViewModel "おおがらす" "9" "0" "10" "6"
+                , MonsterViewModel "さまようよろい" "55" "0" "47" "10"
+                , MonsterViewModel "ドルイド" "35" "10" "55" "29"
+                , MonsterViewModel "はぐれメタル" "6" "∞" "55" "150"
+                , MonsterViewModel "ゾーマ" "4700" "∞" "360" "80"
                 ]
-            , tr []
-                [ td [] [ text "おおがらす" ]
-                , td [] [ text "9" ]
-                , td [] [ text "0" ]
-                , td [] [ text "10" ]
-                , td [] [ text "6" ]
-                ]
-            , tr []
-                [ td [] [ text "さまようよろい" ]
-                , td [] [ text "55" ]
-                , td [] [ text "0" ]
-                , td [] [ text "47" ]
-                , td [] [ text "10" ]
-                ]
-            , tr []
-                [ td [] [ text "ドルイド" ]
-                , td [] [ text "35" ]
-                , td [] [ text "10" ]
-                , td [] [ text "55" ]
-                , td [] [ text "29" ]
-                ]
-            , tr []
-                [ td [] [ text "はぐれメタル" ]
-                , td [] [ text "6" ]
-                , td [] [ text "∞" ]
-                , td [] [ text "55" ]
-                , td [] [ text "150" ]
-                ]
-            , tr []
-                [ td [] [ text "ゾーマ" ]
-                , td [] [ text "4700" ]
-                , td [] [ text "∞" ]
-                , td [] [ text "360" ]
-                , td [] [ text "80" ]
-                ]
-            ]
+        ]
+
+
+monsterViewModel2View : MonsterViewModel -> Html Msg
+monsterViewModel2View { name, hp, mp, attack, agility } =
+    tr []
+        [ td [] [ text name ]
+        , td [] [ text hp ]
+        , td [] [ text mp ]
+        , td [] [ text attack ]
+        , td [] [ text agility ]
         ]
 
 
