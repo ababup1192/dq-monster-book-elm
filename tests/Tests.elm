55a8c3c6c1902c00c11faf0094fc80528636af1c
abab <ababup1192@gmail.com>
2018-10-14 12:47:02 +0900

    [Refactoring] Refactor monster 2 view model test

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -6,40 +6,31 @@ import Main exposing (Monster, MonsterViewModel, infinity, monster2ViewModel)
 import Test exposing (..)
 
 
+type alias TestCaseName =
+    String
+
+
+monster2ViewModelTest : TestCaseName -> Monster -> MonsterViewModel -> Test
+monster2ViewModelTest testCaseName monster monsterViewModel =
+    test testCaseName <|
+        \_ ->
+            let
+                actual =
+                    monster2ViewModel <| monster
+
+                expected =
+                    monsterViewModel
+            in
+            Expect.equal actual expected
+
+
 suite : Test
 suite =
     describe "The Main module"
         [ describe "monster2ViewModel"
             -- Nest as many descriptions as you like.
-            [ test "スライム は ViewModel になった！" <|
-                \_ ->
-                    let
-                        actual =
-                            monster2ViewModel <| Monster "スライム" 8 0 9 4
-
-                        expected =
-                            MonsterViewModel "スライム" "8" "0" "9" "4"
-                    in
-                    Expect.equal actual expected
-            , test "はぐれメタル は ViewModel になった！" <|
-                \_ ->
-                    let
-                        actual =
-                            monster2ViewModel <| Monster "はぐれメタル" 6 infinity 55 150
-
-                        expected =
-                            MonsterViewModel "はぐれメタル" "6" "∞" "55" "150"
-                    in
-                    Expect.equal actual expected
-            , test "ゾーマ は ViewModel になった！" <|
-                \_ ->
-                    let
-                        actual =
-                            monster2ViewModel <| Monster "ゾーマ" 4700 infinity 360 80
-
-                        expected =
-                            MonsterViewModel "ゾーマ" "4700" "∞" "360" "80"
-                    in
-                    Expect.equal actual expected
+            [ monster2ViewModelTest "スライム は ViewModel になった！" (Monster "スライム" 8 0 9 4) (MonsterViewModel "スライム" "8" "0" "9" "4")
+            , monster2ViewModelTest "はぐれメタル は ViewModel になった！(無限に注意!)" (Monster "はぐれメタル" 6 infinity 55 150) (MonsterViewModel "はぐれメタル" "6" "∞" "55" "150")
+            , monster2ViewModelTest "ゾーマ は ViewModel になった！(無限に注意！)" (Monster "ゾーマ" 4700 infinity 360 80) (MonsterViewModel "ゾーマ" "4700" "∞" "360" "80")
             ]
         ]
